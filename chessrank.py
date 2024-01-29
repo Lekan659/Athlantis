1f529d2ce64c9932e8ec0eb9c414756fdc86afb6
OLALEKAN OGUNDELE <Olalekan Ogundele@oakbank.com>
2024-01-29 16:51:29 +0100

    first commit

---
+++ chessrank.py
@@ -0,0 +1,20 @@
+def print_top_50_classical_players() -> None:
+    import requests
+# An example of how to use requests
+    resp = requests.get('https://lichess.org/api/player/top/100/classical')
+    if resp.status_code != 200:
+        return print('Api reurning error', str(resp.status_code))
+    data = resp.json()
+
+    if not data:
+        return print('No data here')
+    
+    print("Food", data)
+
+    for i in range(len(data['users'])):
+        print(data['users'][i]['username'])
+        if i == 49:
+            break
+
+
+print_top_50_classical_players()
